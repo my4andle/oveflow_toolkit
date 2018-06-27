dc46852be65fda14d4730d148e8eee924cc7a7f5
Marc Brotherson <33820078+my4andle@users.noreply.github.com>
2018-06-27 16:42:14 -0600

    Update shellcode.py

--- shellcode.py
+++ shellcode.py
@@ -1,8 +1,13 @@
 # https://github.com/rapid7/metasploit-framework/blob/master/modules/exploits/windows/mssql/ms02_039_slammer.rb
-ms02_039 = (
-"Put your EXPLOIT CODE HERE and it can be imported into the framework for future use."
-"You will need to piece together the exploit code by reverse engineering known exploits such as msf or exploit-db."
-"Once you have working exploit code you can reuse here, but do not forget to create a handler for the specific shellcode."
-"This will help when you can not use metasploit."
-"Examples to come"
-)
+#Put your EXPLOIT CODE HERE and it can be imported into the framework for future use.
+#You will need to piece together the exploit code which includes your shellcode in conjunction with any other bytes required to trigger an overflow.  You can do this by reverse engineering known exploits found in msf or exploit-db.
+#Once you have working exploit code you can reuse here, but do not forget to create a handler for the specific shellcode.
+#This will help when you can not use metasploit.
+#Add print_status(Rex::Text.to_hex(buf)) to msf exploit to print your complete exploit code when executing
+#use msfvenom to create shellcode, and then combine this with other required exploit code
+
+
+ms02_039 = ("\x04\x41......."
+"\x90\x90.........")
+
+
