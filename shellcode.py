0e6a61893619b6d85f57aea0b6b0c50484773db0
Marc Brotherson <33820078+my4andle@users.noreply.github.com>
2018-06-27 16:27:10 -0600

    Create shellcode.py

---
+++ shellcode.py
@@ -0,0 +1,8 @@
+# https://github.com/rapid7/metasploit-framework/blob/master/modules/exploits/windows/mssql/ms02_039_slammer.rb
+ms02_039 = (
+"Put your EXPLOIT CODE HERE and it can be imported into the framework for future use."
+"You will need to piece together the exploit code by reverse engineering known exploits such as msf or exploit-db."
+"Once you have working exploit code you can reuse here, but do not forget to create a handler for the specific shellcode."
+"This will help when you can not use metasploit."
+"Examples to come"
+)
